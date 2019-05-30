221789fde96db5c3f290c47d5cb1d62eeb91fc64
Cesar Varela <cesar@botsfactory.io>
2019-05-30 14:41:23 -0300

    Connect to redis from integration test

--- test/integration_test.js
+++ test/integration_test.js
@@ -2,6 +2,7 @@ const path = require('path');
 const grpc = require('grpc');
 const should = require('should');
 const async = require('async');
+const redis = require('redis');
 
 const HOST = process.env.HOST || 'localhost';
 const PORT = process.env.PORT || 50051;
@@ -10,55 +11,76 @@ const counterProto = grpc.load(PROTO_PATH);
 
 describe('integration tests', () => {
 
-  let client;
+    let client;
 
-  before(() => {
-    // Create a grpc client.
-    // This assumes that the server is running.
-    client = new counterProto.counter.CounterService(
-      HOST + ':' + PORT,
-      grpc.credentials.createInsecure()
-    );
-  });
-
-  afterEach((done) => {
-    client.reset({}, done);
-  });
-
-  it('should reset, then add to the counter', (done) => {
-    async.series({
-      reset: function(callback) {
-        client.reset({}, callback);
-      },
-      add: function(callback) {
-        client.add({count: 1}, callback);
-      },
-      get: function(callback) {
-        client.get({}, (err, res) => {
-          should.not.exist(err);
-          res.count.should.equal(1);
-          callback();
-        });
-      }
-    }, done);
-  });
-
-  it('should watch the counter', (done) => {
-    let stream = client.watch({});
-
-    // want is the wanted sequence of counter.
-    // the last 0 is for the afterEach reset.
-    const want = [2, -1, 0];
-
-    let i = 0;
-    stream.on('data', (got) => {
-      got.count.should.equal(want[i]);
-      i += 1;
+    before(() => {
+        // Create a grpc client.
+        // This assumes that the server is running.
+        client = new counterProto.counter.CounterService(
+            HOST + ':' + PORT,
+            grpc.credentials.createInsecure()
+        );
     });
 
-    async.each([2, -3], function(increment, next) {
-      client.add({count: increment}, next);
-    }, done);
-  });
+    afterEach((done) => {
+        client.reset({}, done);
+    });
+
+    it('should connect to redis', async (done) => {
+
+        // Redis client.
+        const opts = {
+            host: process.env.REDIS_HOST || '127.0.0.1',
+            port: process.env.REDIS_PORT || 6379
+        }
+
+        const redisClient = redis.createClient(opts)
+
+        redisClient.on('connect', () => {
+
+            done()
+        })
+
+        redisClient.on('error', (err) => {
+
+            throw err
+        })
+    })
+
+    it('should reset, then add to the counter', (done) => {
+        async.series({
+            reset: function (callback) {
+                client.reset({}, callback);
+            },
+            add: function (callback) {
+                client.add({ count: 1 }, callback);
+            },
+            get: function (callback) {
+                client.get({}, (err, res) => {
+                    should.not.exist(err);
+                    res.count.should.equal(1);
+                    callback();
+                });
+            }
+        }, done);
+    });
+
+    it('should watch the counter', (done) => {
+        let stream = client.watch({});
+
+        // want is the wanted sequence of counter.
+        // the last 0 is for the afterEach reset.
+        const want = [2, -1, 0];
+
+        let i = 0;
+        stream.on('data', (got) => {
+            got.count.should.equal(want[i]);
+            i += 1;
+        });
+
+        async.each([2, -3], function (increment, next) {
+            client.add({ count: increment }, next);
+        }, done);
+    });
 
 });
